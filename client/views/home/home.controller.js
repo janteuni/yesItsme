d09abf6dadb90cef218e6eb300f438724b3dc1ab
janteuni <janteuni@student.42.fr>
2015-04-15 16:28:30 +0200

    feat(project): add project link

--- client/views/home/home.controller.js
+++ client/views/home/home.controller.js
@@ -8,18 +8,36 @@ angular.module('perso')
 
     TweenLite.defaultEase = Power0.easeNone;
 
+    var busAnim = null;
+    var planeAnim = null;
     var plane = $('#plane');
-    var width = $window.innerWidth;
     var bus = $('#bus');
-    var planeAnim = new TimelineMax({ repeat: -1 });
 
-    planeAnim
-      .to(plane, 10, {x: - width - 500});
+    function draw() {
+      if (busAnim) {
+        busAnim.kill();
+      }
+      if (planeAnim) {
+        planeAnim.kill();
+      }
+      var width = window.innerWidth;
+      planeAnim = new TimelineMax({ repeat: -1 });
 
-    var busAnim = new TimelineMax({ repeat: -1 });
+      planeAnim
+        .set(plane, { x: 500 })
+        .to(plane, 10, {x:  -width});
 
-    busAnim
-      .to(bus, 10, {x: width + 250});
+      busAnim = new TimelineMax({ repeat: -1 });
+
+      busAnim
+        .set(bus, { x: 0 })
+        .to(bus, 10, {x: width + 250});
+    }
+
+    draw();
+
+
+    $(window).resize(draw);
 
     angular.extend(vm, {
     });
